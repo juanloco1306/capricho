7f85a48726b93f57ef8e4e26d556329f46ee3932
juanloco1306 <juanloco1306@gmail.com>
2022-11-25 14:49:39 -0600

    2.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,15 +59,68 @@ let swiperPopular = new Swiper(".popular__container",{
 });
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
+let mixerFeatured = mixitup('.featured__content', {
+    selectors:{
+        target: '.featured__card'
+    },
+    animation:{
+        duration:300
+    }
+})
 
 
 /* Link active featured */ 
+const linkFeatured = document.querySelectorAll('.featured__items')
+
+function activeFeatured(){
+    linkFeatured.forEach(l => l.classList.remove('active-featured'))
+    this.classList.add('active-featured')
+}
+linkFeatured.forEach(l=> l.addEventListener('click', activeFeatured))
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 1350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal('.home__title, .popular__container')
+sr.reveal('.home__subtitle', {delay: 500})
+sr.reveal('.home__img', {delay: 800})
+sr.reveal('.home__button', {delay: 1000, interval: 100, origin: 'bottom'})
+sr.reveal('.about__group, .featured__filters', {origin: 'left'})
+sr.reveal('.about__data ', {origin: 'right'})
+sr.reveal('.featured__card', {interval: 200})
